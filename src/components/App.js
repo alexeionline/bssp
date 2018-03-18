c193e51cb7249c4148cd7b35ebe11d70ea92b973
xKnowledgeless <kinage159@mail.ru>
2018-03-18 21:47:55 +0300

    Feature add prettier (#4)
    Prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
-import { Container } from 'semantic-ui-react'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
 
-import YandexMap from './YandexMap'
-import Menu from './AppMenu'
-import About from './About'
-import Add from './Add'
-import SomeObject from './SomeObject'
-import PageNotFound from './PageNotFound'
+import YandexMap from "./YandexMap";
+import Menu from "./AppMenu";
+import About from "./About";
+import Add from "./Add";
+import SomeObject from "./SomeObject";
+import PageNotFound from "./PageNotFound";
 
 const App = () => {
-    return (
-        <Router>
-            <Container>
-                <Container>
-                    <Menu/>
-                </Container>
-                <Container>
-                    <Switch>
-                        <Route exact path = "/" component = { YandexMap }/>
-                        <Route path = "/about" component = { About }/>
-                        <Route path = "/add" component = { Add }/>
-                        <Route path = "/o/:objectID" component = { SomeObject }/>
-                        <Route component = { PageNotFound }/>
-                    </Switch>
-                </Container>
-            </Container>
-        </Router>
-    );
+  return (
+    <Router>
+      <Container>
+        <Container>
+          <Menu />
+        </Container>
+        <Container>
+          <Switch>
+            <Route exact path="/" component={YandexMap} />
+            <Route path="/about" component={About} />
+            <Route path="/add" component={Add} />
+            <Route path="/o/:objectID" component={SomeObject} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </Container>
+      </Container>
+    </Router>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 const store = createStore(() => {
-    //сap
+  //сap
 });
 
 ReactDOM.render(
-    <Provider store = { store }>
-        <App/>
-    </Provider>
-    , document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
