79873a403a2ba823f0de75e698a3b993985bbd7d
konstantinpodmasko <podmasko.konstantin@gmail.com>
2018-03-19 20:54:31 +0300

    fix variable name

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const fileSystem = require('fs');
 const app = express();
 const objectsJSON = fileSystem.readFileSync('./db.json');
