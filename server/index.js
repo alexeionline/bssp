435082a79485b37d9ea86e045c6fb72b72f24075
konstantinpodmasko <podmasko.konstantin@gmail.com>
2018-03-19 20:30:47 +0300

    add api/objects

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const fileStream = require("fs");
 const app = express();
+const objectsJSON = fileStream.readFileSync('./db.json');
+const objects = JSON.parse( objectsJSON ).objects;
+
+app.get("/api/objects/",
+    function (req, res) {
+        res.json(objects);
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
