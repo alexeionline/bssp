4ab14ea6304f77504cf951e407022e9cb5acfc25
xKnowledgeless <kinage159@mail.ru>
2018-03-19 21:51:10 +0300

    add api/objects/:objectID (#9)

--- server/index.js
+++ server/index.js
@@ -8,6 +8,13 @@ app.get("/api/objects/", function(req, res) {
   res.json(objects);
 });
 
+app.get("/api/objects/:objectID", function(req, res) {
+  const currentObject = objects.filter(
+    item => +req.params.objectID === item.id
+  );
+  res.json(currentObject);
+});
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
