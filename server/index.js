1ad41c6dee8af3e31678512f6f714f2fb2baa908
konstantinpodmasko <podmasko.konstantin@gmail.com>
2018-03-21 23:11:23 +0300

    prettier

--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
 const express = require("express");
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 const app = express();
 
-
-const adapter = new FileSync('./db.json');
+const adapter = new FileSync("./db.json");
 const db = low(adapter);
 
-
 app.get("/api/objects/", function(req, res) {
-  res.json(db.get('objects'));
+  res.json(db.get("objects"));
 });
 
 app.get("/api/objects/:objectID", function(req, res) {
-  const currentObject = db.get('objects').filter(
-    item => +req.params.objectID === item.id
-  );
+  const currentObject = db
+    .get("objects")
+    .filter(item => +req.params.objectID === item.id);
   res.json(currentObject);
 });
 
